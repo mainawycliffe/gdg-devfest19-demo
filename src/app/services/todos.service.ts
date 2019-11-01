1e0f07416e68ddb7e2701c792a051c53b4f42d4f
Maina Wycliffe <wmmaina7@gmail.com>
2019-11-02 00:46:55 +0300

    switch to behavior subject for change detection

--- src/app/services/todos.service.ts
+++ src/app/services/todos.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Todos, Todo } from '../todos';
+import { BehaviorSubject, Observable, from, ReplaySubject, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -12,17 +13,33 @@ export class TodosService {
     { isDone: false, todo: 'go to school' }
   ];
 
+  todos$: BehaviorSubject<Todos> = new BehaviorSubject(this.todos);
+
   constructor() {}
 
-  getTodos(): Todos {
-    return this.todos;
+  getTodos(): Observable<Todos> {
+    return this.todos$;
   }
 
   addTodo(todo: Todo) {
     this.todos.push(todo);
+
+    // notify listeners
+    this.todos$.next(this.todos);
   }
 
-  isDone(index: number) {
-    this.todos[index].isDone = true;
+  toggleTodoStatus(todo: Todo) {
+    this.todos = this.todos.map(v => {
+      if (todo.todo === v.todo) {
+        return {
+          isDone: !todo.isDone,
+          todo: todo.todo
+        };
+      }
+      return v;
+    });
+
+    // notify listeners
+    this.todos$.next(this.todos);
   }
 }
