297b212a8ae182d86269295b5cc25dd245c4e9c5
Maina Wycliffe <wmmaina7@gmail.com>
2019-11-02 01:58:22 +0300

    remove routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Injector } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { createCustomElement } from '@angular/elements';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListTodosComponent } from './todos/list-todos/list-todos.component';
 import { AddTodoComponent } from './todos/add-todo/add-todo.component';
 
 @NgModule({
   declarations: [AppComponent, ListTodosComponent, AddTodoComponent],
-  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
+  imports: [BrowserModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  entryComponents: [ListTodosComponent, AddTodoComponent]
+  // bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private injector: Injector) {}
+
+  ngDoBootstrap() {
+    const elements: any[] = [
+      ['add-todo', AddTodoComponent],
+      ['list-todos', ListTodosComponent]
+    ];
+
+    for (const [name, component] of elements) {
+      const el = createCustomElement(component, { injector: this.injector });
+      customElements.define(name, el);
+    }
+  }
+}
